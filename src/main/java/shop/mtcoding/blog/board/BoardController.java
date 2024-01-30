e0c1bcc28621e79bf687f9eee36cf979eaa634e0
yun <yunnn687@naver.com>
2024-01-30 17:56:56 +0900

    로그인 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,13 +1,28 @@
 package shop.mtcoding.blog.board;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import shop.mtcoding.blog.user.User;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
 
+    private final HttpSession session;
+
     @GetMapping({ "/", "/board" })
     public String index() {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+//        if (session == null) {
+//            System.out.println("로그인 안 된 상태입니다.");
+//        } else {
+//            System.out.println("로그인 된 상태입니다.");
+//        }
+
         return "index";
     }
 
--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,42 +1,68 @@
 package shop.mtcoding.blog.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 
 /**
- * 컨트롤러
- * 1. 요청받기 (URL)
- * 2. http body 데이터는 어떻게? (DTO로 받음)
- * 3. 기본 mime 전략 : x-www-form-urlencoded (username=ssar&password=1234)
- * 4. 유효성 검사하기 (body 데이터가 있다면)
- * 5. 클라이언트가 view(mustache 파일)만 원하는지? 혹은 DB 처리 후
- * 6. view만 원하면 view를 응답하면 끝
- * 7. DB 처리를 원하면 Model(DAO)에게 위임 후 view를 응답하면 끝
+ *  컨트롤러
+ *  1. 요청받기 (URL - URI 포함)
+ *  2. 데이터(http bodt)는 어떻게? (DTO)
+ *  3. 기본 mime 전략 : x-www-form-urlencoded (username=ssar&password=1234)
+ *  4. 유효성 검사하기 (body 데이터가 있다면)
+ *  5. 클라이언트가 View만 원하는지? 혹은 DB 처리 후 View도 원하는지?
+ *  6. View만 원하면 view를 응답하면 끝
+ *  7. DB처리를 원하면 Model(DAO)에게 위임 후 view를 응답하면 끝
  */
-
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final HttpSession session;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO requestDTO){
+
+        // 1. 유효성 검사
+        if(requestDTO.getUsername().length() < 3){
+            return "error/400";
+        }
+
+        // 2. Model 필요 select * from user_tb where username=? and password=?
+        User user = userRepository.findByUsernameAndPassword(requestDTO);
+        System.out.println(user);
+
+        if (user == null) {
+            return "error/401";
+        } else {
+            session.setAttribute("sessoionUser", user);
+            return "redirect:/";
+        }
+
+
+        // 3. 응답
+
     }
 
-    @PostMapping("join")
-    public String join(UserRequest.JoinDTO requestDTO) {
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO requestDTO){
         System.out.println(requestDTO);
 
         // 1. 유효성 검사
-        if (requestDTO.getUsername().length() < 3) {
+        if(requestDTO.getUsername().length() < 3){
             return "error/400";
         }
-        
-        // 2. Model 에게 위임하기
-        userRepository.saveV2(requestDTO);
 
+        // 2. Model에게 위임하기
+        userRepository.save(requestDTO);
+
+        // 3. 응답
         return "redirect:/loginForm";
     }
 
