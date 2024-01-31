f174b11ce17581f01b5faf9a06d91e1cb1664142
yun <yunnn687@naver.com>
2024-01-31 09:37:41 +0900

    [fix] 로그인 완료

--- src/main/java/shop/mtcoding/blog/user/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -37,8 +37,12 @@ public class UserRepository {
         Query query = em.createNativeQuery("select * from user_tb where username=? and password=?", User.class);
         query.setParameter(1, requestDTO.getUsername());
         query.setParameter(2, requestDTO.getPassword());
+        try {
+            User user = (User) query.getSingleResult();
+            return user;
+        } catch (Exception e) {
+            return null;
+        }
 
-        User user = (User) query.getSingleResult();
-        return user;
     }
 }
