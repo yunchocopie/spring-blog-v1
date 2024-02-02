1091aeae029c44a6115eb1419eed09dbc746d11b
yun <yunnn687@naver.com>
2024-02-02 18:22:18 +0900

    [fix] JPA ORM 최신

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -4,29 +4,31 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import shop.mtcoding.blog.user.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
 
     private final HttpSession session;
+    private final BoardRepository boardRepository;
+
+    // 데이터 리턴시 (RestController or ResponseBody) - 객체를 리턴하면 스프링이 json으로 변환
+    @GetMapping("/api/board/{id}")
+    public @ResponseBody Board apiBoard(@PathVariable int id){
+        return boardRepository.findById(id);
+    }
+
 
     @GetMapping({ "/", "/board" })
     public String index() {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-
-//        if (session == null) {
-//            System.out.println("로그인 안 된 상태입니다.");
-//        } else {
-//            System.out.println("로그인 된 상태입니다.");
-//        }
-
         return "index";
     }
 
     @GetMapping("/board/saveForm")
     public String saveForm() {
+
         return "board/saveForm";
     }
 
